d17202404e7b4fbe08775882e628002985bd5788
Sadatul <sadatulislamsadi@gmail.com>
2023-08-15 21:04:10 +0600

    Added class, questioncount, difficulty and subject choice to index.js, need to add chapter choice and topic choice

--- src/pages/exam/index.js
+++ src/pages/exam/index.js
@@ -1,66 +1,112 @@
 import Link from 'next/link'
 import { Form } from 'react-bootstrap';
 import { useState } from 'react';
+import { runQueryFromFile } from '@/utils/runQuery';
 
 const initialExamData = {
+    questionCount: -1,
+    difficulty: -1,
     class: "",
     subject: "",
-    questionCount: 20,
-    chapter: -1,
-    topic: -1,
-    difficulty: 1,
+    chapter: "",
+    topic: "",
 };
 
 const subjects = ["Bangla", "English"];
 
-export default function examHome() {
+export default function examHome({ repo }) {
+    const topicInfo = repo.rows;
+    // console.log(topicInfo);
 
     const [examData, setExamData] = useState(initialExamData);
     console.log(examData);
+
+    // Repo stores the entire topic info table.
+    // Now will extract data using normal JS.
+
+    // Extracting all unique the classes from the topicInfo table.
+    let classList = new Set();
+    for (let i = 0; i < topicInfo.length; i++) {
+        classList.add(topicInfo[i].class);
+    }
+    let classOptionList = [];
+    for (let item of classList) {
+        classOptionList.push(<option value={item}>{item}</option>);
+    }
+
+    // Extracting all unique the subjects from the topicInfo table under the currently selected class.
+
+    let subjectList = new Set();
+    let subjectOptionList = [];
+    if (examData.class != "") {
+        for (let i = 0; i < topicInfo.length; i++) {
+            if (topicInfo[i].class == examData.class) {
+                subjectList.add(topicInfo[i].subject);
+            }
+        }
+        for (let item of subjectList) {
+            subjectOptionList.push(<option value={item}>{item}</option>);
+        }
+    }
 
     return (
         <div>
             <Form>
                 <Form.Group controlId="difficult">
-                    <Form.Label className='mr-10'>Select Difficulty</Form.Label>
-                    <Form.Select className=" border-2 border-gray-700" aria-label="Default select example"
+                    <Form.Select aria-label="Default select example"
                         id='eh-difficulty' // eh = exam home
                         onChange={(e) => {
                             let tmp = { ...examData };
                             tmp.difficulty = Number(e.target.value);
                             setExamData(tmp);
                         }}>
+                        <option value="-1">Select Difficulty Level</option>
                         <option value="1">Easy</option>
                         <option value="2">Medium</option>
                         <option value="3">Hard</option>
                     </Form.Select>
                 </Form.Group>
                 <Form.Group controlId="questionCount">
-                    <Form.Label className='mr-10'>Select Question Count</Form.Label>
-                    <Form.Select className=" border-2 border-gray-700" aria-label="Default select example"
+                    <Form.Select aria-label="Default select example"
                         id='eh-questionCount' // eh = exam home
                         onChange={(e) => {
                             let tmp = { ...examData };
                             tmp.questionCount = Number(e.target.value);
                             setExamData(tmp);
                         }}>
+                        <option value="-1">Select Question Count</option>
                         <option value="20">20</option>
                         <option value="40">40</option>
                         <option value="100">100</option>
                     </Form.Select>
                 </Form.Group>
                 <Form.Group controlId="class">
-                    <Form.Label className='mr-10'>Select Class</Form.Label>
-                    <Form.Select className=" border-2 border-gray-700" aria-label="Default select example"
+                    <Form.Select aria-label="Default select example"
                         id='eh-class' // eh = exam home
                         onChange={(e) => {
                             let tmp = { ...examData };
-                            tmp.questionCount = Number(e.target.value);
+                            tmp.class = e.target.value;
+                            tmp.subject = "";
+                            tmp.chapter = "";
+                            tmp.topic = "";
                             setExamData(tmp);
                         }}>
-                        <option value="20">20</option>
-                        <option value="40">40</option>
-                        <option value="100">100</option>
+                        <option value="">Select Class</option>
+                        {classOptionList}
+                    </Form.Select>
+                </Form.Group>
+                <Form.Group key={examData.class} controlId="subject">
+                    <Form.Select aria-label="Default select example"
+                        id='eh-subject' // eh = exam home
+                        onChange={(e) => {
+                            let tmp = { ...examData };
+                            tmp.subject = e.target.value;
+                            tmp.chapter = "";
+                            tmp.topic = "";
+                            setExamData(tmp);
+                        }} disabled={examData.class == ""}>
+                        <option value="">Select Subject</option>
+                        {subjectOptionList}
                     </Form.Select>
                 </Form.Group>
             </Form>
@@ -73,4 +119,12 @@ export default function examHome() {
             {/* <Exam examData={examData} /> */}
         </div>
     )
+}
+
+export const getStaticProps = async () => {
+    const res = await runQueryFromFile('getAllTopics');
+    const repo = {
+        rows: res
+    }
+    return { props: { repo } }
 }
